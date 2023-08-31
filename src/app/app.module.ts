81a04922287fc3aedde2286865e5249149673a24
shota shavadze <shot.shavadze@gmail.com>
2023-08-31 15:36:18 +0400

    delete entire code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent
+    AppComponent
  ],
   imports: [
     BrowserModule,
